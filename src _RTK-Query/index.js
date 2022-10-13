4232be90f0eacec77ad769301d713d47960f491b
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-10-13 17:55:49 +0300

    Добавлен макет из конспекта

--- src _RTK-Query/index.js
+++ src _RTK-Query/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-// import 'modern-normalize';
-import './styles.css';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'redux/store';
+
+
+import { App } from 'components/App/App';
+
+import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,64 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
-// import { combineReducers } from 'redux'; //? уже не надо с RTK Query
-
-import { filterSlice } from 'redux/filter/filterSlice';
-
-import { itemsAPIbyRTKQuery } from 'redux/items/itemsSliceRTKQuery';
-import { authAPIbyRTKQuery } from 'redux/auth/authSliceRTKQuery';
-
-//? +++++++++++++++++++++++++++++++ redux-persist +++++++++++++++++++++++++++++++
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import {
+    persistStore,
+    persistReducer,
     FLUSH,
     REHYDRATE,
     PAUSE,
     PERSIST,
     PURGE,
     REGISTER,
-} from 'redux-persist'
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { tasksReducer } from './tasks/slice';
+import { authReducer } from './auth/slice';
 
+const middleware = [
+    ...getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+];
+
+// Persisting token field from auth slice to localstorage
 const authPersistConfig = {
     key: 'auth',
     storage,
     whitelist: ['token'],
 };
-//? _______________________________ redux-persist _______________________________
 
-// import { todosReducer } from './todos';
-// import { authReducer } from './auth';
-
-//! +++++++++++ persistedItemsReducer with redux-persist +++++++++++++++
-const persistedAuthReducer = persistReducer(authPersistConfig, authAPIbyRTKQuery.reducer);
-
-
-
-//! +++++++++++ store with RTK Query (3-й вариант - РАБОТАЕТ!!!) +++++++++++++++
-//! export const getFilter = state => state.filter;
 export const store = configureStore({
     reducer: {
-        [itemsAPIbyRTKQuery.reducerPath]: itemsAPIbyRTKQuery.reducer,
-        filter: filterSlice.reducer,
-        // [authAPIbyRTKQuery.reducerPath]: authAPIbyRTKQuery.reducer,
-        auth: persistedAuthReducer
+        auth: persistReducer(authPersistConfig, authReducer),
+        tasks: tasksReducer,
     },
-    // middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), itemsAPIbyRTKQuery.middleware],
-    middleware: (getDefaultMiddleware) => [
-        ...getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
-        itemsAPIbyRTKQuery.middleware,
-        authAPIbyRTKQuery.middleware,
-
-    ],
+    middleware,
+    devTools: process.env.NODE_ENV === 'development',
 });
 
-//! +++++++++++ with redux-persist +++++++++++++++
-export const persistor = persistStore(store)
-
-
-//! ++++++++++++++++++++++++++++ ВЕСЬ State +++++++++++++++++++++++++++++++++++
-// console.log("ВЕСЬ State из store.js ==> store.getState():", store.getState()); //!
-//! ____________________________________________________________________________
+export const persistor = persistStore(store);
